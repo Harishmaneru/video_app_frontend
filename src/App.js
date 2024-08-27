8d38e89dc2d206bab6d514c85cd8f634b2a6c7f2
YourUsername <harishmaneru44@gmail.com>
2024-08-27 14:32:34 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { BrowserRouter, Routes, Route , Navigate} from 'react-router-dom';
 import VideoRecorder from './components/VideoRecorder';
 
 const App = () => {
+  
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/application" element={<VideoRecorder />} />
-        <Route path="/*" element={<VideoRecorder />} />
+        <Route exact path="/application" element={<VideoRecorder />} />
+        <Route path="*" element={<Navigate to="/application" />} />
+        <Route path="/" element={<Navigate to="/application" />} />
       </Routes>
     </BrowserRouter>
   );
